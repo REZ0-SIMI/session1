256da8f7d12586bd524b6f574669c380eca485a1
REZ0-SIMI <rezoanasimthy97@gmail.com>
2024-05-29 15:25:48 +0600

    Create session2.py
    fr

---
+++ session2.py
@@ -0,0 +1,13 @@
+# lets assume some xyz company three products
+phones = 1400   # 10, consider as first product
+laptop = 2000   # 20, consider as second product
+watch = 1500    # 8, consider as third product
+airpod = 200   # 13,consider as fourth product
+tv = 6000      # 10, consider as fifth product
+st_phones = phones*0.1
+st_laptop = laptop* 0.2
+st_watch = watch*1.2
+st_airpod = airpod*0.13
+st_tv = tv*0.1
+print(st_phones,st_laptop,st_watch,st_airpod,st_tv)
+print(st_phones+phones,st_laptop+laptop,st_watch+watch,st_airpod+airpod,st_tv+tv)
